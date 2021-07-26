15b4c1122f6d9ceddefd93b8a9c7b5afb2974136
Baba Fakruddin <fakruddinbaba16@gmail.com>
2021-07-27 01:08:23 +0530

    simple chnages

--- Financial-Spend-Analyzer/src/components/home/home.component.ts
+++ Financial-Spend-Analyzer/src/components/home/home.component.ts
@@ -21,11 +21,42 @@ myform = new FormGroup({
   }
   onSubmit(){
     let d = new Date();
-   console.log(this.myform.value);
+    let month;
+    switch(d.getMonth()){
+      case 0: month = "january"
+      break;
+      case 1: month = "Febraury"
+      break;
+      case 2: month = "March"
+      break;
+      case 3: month = "April"
+      break;
+      case 4: month = "May"
+      break;
+      case 5: month = "June"
+      break;
+      case 6: month = "July"
+      break;
+      case 7: month = "Aug"
+      break;
+      case 8: month = "September"
+      break;
+      case 9: month = "October"
+      break;
+      case 10: month = "November"
+      break;
+      case 11: month = "December"
+      break;
+      default: month = "month doesn't exist"
+
+    }
    let data = 
-    { id : this.id,
+    { [month] : {
+   customerNumber : this.id,
       Transaction: this.myform.value,
-      date : d
+      date : d,
+      payment: "debit"  }
+      
      }  
    this.financial.post(data).subscribe((data:any) => {
      console.log(data)
